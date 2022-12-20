ba60d82eb4a27df0eb71a30d9a9c65535fcf2816
lang92 <74031598+lang92@users.noreply.github.com>
2022-12-20 22:17:23 +0900

    Update Router.js

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
 
 export default function Router() {
   return (
