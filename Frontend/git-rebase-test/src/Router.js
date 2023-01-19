3bdddca2e3b38af4f17fbe5b4f83b6e95ceb4d04
Hailey_ys <whassup_man@naver.com>
2023-01-19 14:14:10 +0900

    Modify: login, main router 경로 수정

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -7,8 +7,13 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path='/' element={<Main />} />
         <Route path='/signin' element={<Login />} />
+=======
+        <Route path='/login' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+>>>>>>> 01d1716 (Modify: login, main router 경로 수정)
       </Routes>
     </BrowserRouter>
   );
