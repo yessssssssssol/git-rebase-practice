6cc0093c3ae0f209edc0f215690a1f7b5f1d0d2c
Hailey_ys <whassup_man@naver.com>
2023-01-19 10:57:39 +0900

    Add: login 페이지 레이아웃 완료

--- Frontend/git-rebase-test/src/pages/Login/Login.js
+++ Frontend/git-rebase-test/src/pages/Login/Login.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
 
-function Login() {
+export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className='login'>
+      <main className='loginContainer'>
+        <h1 className='title'>Git Rebase Test</h1>
+        <form className='loginForm'>
+          <input
+            className='userId'
+            type='text'
+            placeholder='이메일을 입력해 주세요.'
+          />
+          <input
+            className='userPassword'
+            type='password'
+            placeholder='비밀번호를 입력해 주세요.'
+          />
+          <button className='loginButton' type='button'>
+            로그인
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
-
-export default Login
